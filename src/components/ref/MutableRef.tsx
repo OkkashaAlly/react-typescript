6cacb0661639480a4a706ccfb64189925a3102ee
OkkashaAlly <steamnature006@gmail.com>
2023-01-08 11:11:36 +0300

    Use ref Mutable refference

--- src/components/ref/MutableRef.tsx
+++ src/components/ref/MutableRef.tsx
@@ -1,9 +1,25 @@
-import {useRef, useEffect} from 'react'
+import { useRef, useState, useEffect } from "react";
 
 const MutableRef = () => {
-  return (
-    <div>MutableRef</div>
-  )
-}
+  const [timer, setTimer] = useState(0);
+  const intervalRef = useRef<number | undefined>(undefined);
 
-export default MutableRef
+  const stopTimer = () => window.clearInterval(intervalRef.current);
+
+  useEffect(() => {
+    intervalRef.current = window.setInterval(() => {
+      setTimer(timer => timer + 1);
+    }, 1000);
+
+    return () => stopTimer();
+  }, []);
+
+  return (
+    <div>
+      <h1>HOok timer - {timer}</h1>
+      <button onClick={() => stopTimer()}>stop timer</button>
+    </div>
+  );
+};
+
+export default MutableRef;
